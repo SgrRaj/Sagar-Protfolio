04a81a7d04ee9c7ea56b9886043a279e1830dc8c
Sagar Raj <sgrraj96@gmail.com>
2024-09-20 23:10:56 +0530

    today

---
+++ script.js
@@ -0,0 +1,16 @@
+
+window.onscroll = function() { scrollFunction(); };
+
+function scrollFunction() {
+    var scrollBtn = document.getElementById("scrollBtn");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        scrollBtn.style.display = "block";
+    } else {
+        scrollBtn.style.display = "none";
+    }
+}
+
+
+
+
+
