9e492aca2a74bb0ea7646d57bf9f4f382a9a325f
Sagar Raj <sgrraj96@gmail.com>
2024-09-22 14:29:01 +0530

    updated

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-
+// When the user scrolls down 100px from the top of the document, show the button
 window.onscroll = function() { scrollFunction(); };
 
 function scrollFunction() {
     var scrollBtn = document.getElementById("scrollBtn");
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        scrollBtn.style.display = "block";
+        scrollBtn.style.display = "block"; // Show button
     } else {
-        scrollBtn.style.display = "none";
+        scrollBtn.style.display = "none";  // Hide button
     }
 }
+
+// When the user clicks on the button, scroll to the top of the document
+document.getElementById("scrollBtn").onclick = function() {
+   
+    document.documentElement.scrollTop = 0; 
+};
 
 
 
